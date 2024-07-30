f871a24b5633d2cbcaa959f2012dc032e972a799
satyasai69 <ch.satya.sai133@gmail.com>
2024-07-31 00:24:46 +0530

    test

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {DepolyOurToken} from "script/DepolyOurToken.s.sol";
 import {OurToken} from "src/OurToken.sol";
 
-contract OurTokenTest is Test {}
+contract OurTokenTest is Test {
+    function setUp() public {}
+}
